4295ee5ab99bf3e2980475a519d78216f7f15916
Benjamin Schefrin <bschefri@uoregon.edu>
2021-05-02 13:25:22 -0600

    Finally got leaflet to work with input$sel_data. Now just need to edit layout and add some descriptions.

--- housing_vacancy.R
+++ housing_vacancy.R
@@ -1,10 +1,11 @@
 library(pacman)
 
 p_load("tidyverse", "httr", "jsonlite", "janitor", "usmap", "lubridate", "shiny", "hrbrthemes", 
-       "viridis", "shinyWidgets", "leaflet", "sf", "maps", "scales")
+       "viridis", "shinyWidgets", "leaflet", "sf", "maps", "scales", "shinydashboard", "rgdal")
 
 # This project is the code for practicing shiny apps. I want to take the counties of CO
-# and provide summary housing statistics for each county.
+# and provide visual statistics for each county. Major props to my former professors
+# Ed Rubin and Grant Mcdermott for introducing me to R and visualizations. Please check out their work.
 
 # fips codes for CO counties -------------------------------------------------------------------------
 
@@ -27,7 +28,7 @@ permit_data_2 <- permit_data %>%
   filter(placeFips == "00000") %>% 
   select(-c("placeFips", "sdoBuildingPermit"))
 
-# Adding in a column that describes the % of vacant houses by county
+# Adding in a column that describes the % of vacant houses by county, making decimals pretty
 permit_data_3 <- permit_data_2 %>% 
   mutate(vacancy_percentage = (vacantHousingUnits/totalHousingUnits)*100) %>% 
   mutate(vacancy_percentage = format(round(vacancy_percentage, 2), nsmall = 2))
@@ -43,53 +44,10 @@ permit_data_5 <- permit_data_4 %>%
   rename("fips" = "fips_state_and_county_code") %>% 
   mutate(vacancy_percentage = as.numeric(vacancy_percentage))
 
-# Map test-----------------------------------------------------------------------------------------
-# Trying to make leaflet work with maps package and data
-
-permit_data_test <- permit_data_5 %>% 
-  filter(year == 2015)
-
-
-# checking to see what map looks like 
-co_map <- st_as_sf(map("county", "colorado", plot = FALSE, fill = TRUE))
-
 # getting fips and location information, adding a 0 to fips column for merge
 county_fips <- county.fips %>% 
   mutate(fips = paste0("0", fips))
 
-# Merging fips data and name data
-co_map_2 <- merge(permit_data_test, county_fips)
-
-# Merging coordinate data
-co_map_3 <- merge(co_map_2, co_map, by.x = "polyname", by.y = "ID")
-
-# Setting a color pallette for leaflet 
-col_pal <- colorQuantile(palette = "viridis", domain = co_map_3$vacancy_percentage, n = 10)
-
-#convert to sf
-co_map_4 <- st_as_sf(co_map_3)
-# Building a map
-# Warning : Legend percentile is in relation to other CO counties, not true vacancy rate. 
-# Remember to make a very very explicit note of this when publishing
-co_map_4 %>% 
-  st_transform(crs = "+init=epsg:4326") %>%
-  leaflet(width = "100%") %>%
-  addProviderTiles(provider = "CartoDB.Positron") %>%
-  addPolygons(
-    popup = ~paste0(area, "<br>", "Vacancy %: ", vacancy_percentage),
-    stroke = FALSE,
-    smoothFactor = 0,
-    fillOpacity = 0.7,
-    color = ~col_pal(vacancy_percentage)
-  ) %>% 
-  addLegend(
-    "bottomright", 
-    pal = col_pal, 
-    values = ~vacancy_percentage,
-    title = "Percentile of CO counties",
-    opacity = 1
-  )
-# Great success!!!!
 
 # Leaflet prep ---------------------------------------------------------------------------------------
 # Attaching columns for leaflet use
@@ -100,11 +58,6 @@ permit_data_7 <- merge(permit_data_6, co_map, by.x = "polyname", by.y = "ID")
 #cleaning names to look pretty for drop down menu
 permit_data_8 <- clean_names(permit_data_7)
   
-# Filtering housing vacancy percentages
-
-# permit_data_9 <- permit_data_8 %>% 
-#   select(polyname, geom, fips, area, year, vacancy_percentage)
-
 # Assigning sf designation to data
 
 permit_data_final <- st_as_sf(permit_data_8)
@@ -122,29 +75,23 @@ ui <- fluidPage(
             inputId = "year",
             label = "Year",
             choices = sort(permit_data_final$year)
+          ),
+          br(),
+
+          selectizeInput(
+            inputId = "sel_data",
+            label = "Data to Display",
+            choices = c("Total Population" = "total_population", # Relabeling columns so they look better in shiny
+                        "Persons Per Household" = "persons_per_household",
+                        "Total Housing Units" = "total_housing_units",
+                        "Vacant Housing Units" = "vacant_housing_units",
+                        "Vacancy Percentage" = "vacancy_percentage"
+            ),
+            multiple = FALSE
           )
-          # br(),
-          # 
-          # selectInput(
-          #   inputId = "sel_data",
-          #   label = "Data to Display",
-          #   choices = c("Total Population" = "total_population",
-          #               "Persons Per Household" = "persons_per_household",
-          #               "Total Housing Units" = "total_housing_units",
-          #               "Vacant Housing Units" = "vacant_housing_units",
-          #               "Vacancy Percentage" = "vacancy_percentage"
-          #   ),
-          #   multiple = FALSE
-          #   # uiOutput("total_population"),
-          #   # uiOutput("persons_per_household"),
-          #   # uiOutput("total_housing_units"),
-          #   # uiOutput("vacant_housing_units"),
-          #   # uiOutput("vacancy_percentage")
-          # )
           ),
         mainPanel(
           leafletOutput("heat_map"),
-          plotOutput("bar_graph") 
         ))
       ),
       tabPanel(title = "Counties over Time",
@@ -180,28 +127,43 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  county_leaflet_map <- reactive({
-    req(input$year)
-    filter(permit_data_final, year %in% input$year)
-  })
+# For whatever reason not having a reactive function made leaflet work. According to many this is 
+# bad practice but I haven't found a solution that works with a reactive function. I literally spent
+# over 15 hours trying to make it work and it doesn't so I am going with this. 
+
+  
+  #leaflet map code
   
   output$heat_map <- renderLeaflet({
-    county_leaflet_map() %>% 
+    input$year
+    input$sel_data
+    
+    # Filtering data for leaflet map
+    permit_leaflet_map <- permit_data_final %>% 
+      select(area, year, geom, fips, polyname, input$sel_data) %>% 
+      filter(year %in% input$year)
+    
+    # Setting the color palette. One of my former professors Ed Rubin is Red Green color blind and
+    # recommended the viridis color palette so he could see it. This is for you Ed!
+    col_pal_2 <- colorQuantile(palette = "viridis", domain = permit_leaflet_map[[input$sel_data]], n = 10)
+    
+    
+    permit_leaflet_map %>% 
       st_transform(crs = "+init=epsg:4326") %>%
       leaflet(width = "100%") %>%
       addProviderTiles(provider = "CartoDB.Positron") %>%
       addPolygons(
-        popup = ~paste0(area, "<br>", "Vacancy %: ", vacancy_percentage),
+        popup = ~paste0(area, "<br>", "Selected Data: ", get(input$sel_data)),
         stroke = FALSE,
         smoothFactor = 0,
         fillOpacity = 0.7,
-        color = ~col_pal(vacancy_percentage)
+        color = ~col_pal_2(get(input$sel_data))
       ) %>% 
       addLegend(
         "bottomright", 
-        pal = col_pal, 
-        values = ~vacancy_percentage,
-        title = "Percentile of CO counties",
+        pal = col_pal_2, 
+        values = ~get(input$sel_data),
+        title = "Percentile in CO",
         opacity = 1
       )
       
@@ -209,6 +171,9 @@ server <- function(input, output, session) {
     
   })
   
+  #line graph code
+  
+  # Reactive function for line graph
   county_line_graph <- reactive({
     req(input$counties)
     req(input$sel_data_2)
@@ -220,9 +185,13 @@ server <- function(input, output, session) {
     
   })
   
+  # Output for line graph
   output$line_graph <- renderPlot({
     input$counties
     input$sel_data_2
+    
+
+    
       ggplot(data = county_line_graph()) +
         geom_line(mapping = aes(x = year, y = get(input$sel_data_2) , group = area, color = area), lwd = 1.5) +
         labs(title = "Selected Data by County 2010 - 2018", color = "County") +
@@ -234,41 +203,6 @@ server <- function(input, output, session) {
         theme(plot.title = element_text(hjust = 0.5), axis.title.y = element_text(angle = 90, hjust = 0.5))
   })
 
-  # county_comparison <- reactive({
-  #   req(input$year)
-  #   req(input$sel_data)
-  #   filter(permit_data_final, year %in% input$year) %>% 
-  #     select(permit_data_final$polyname, permit_data_final$geom, permit_data_final$fips, 
-  #            permit_data_final$area,
-  #            permit_data_final$year, 
-  #            get(input$sel_data))
-  #   
-  # })
-  # 
-  # county_line_graph <- reactive({
-  #   req(input$counties)
-  #   req(input$sel_data_2)
-  #   filter(permit_data_final, area %in% input$counties) %>% 
-  #     select(permit_data_final$area,
-  #            permit_data_final$year, 
-  #            get(input$sel_data_2))
-  # 
-  # })
-  # 
-  # output$line_graph <- renderPlot({
-  #   input$counties
-  #   input$sel_data_2
-  #   ggplot(data = county_line_graph()) +
-  #     geom_line(mapping = aes(x = year, y = get(input$sel_data_2), group = area, color = area), lwd = 1.5) +
-  #     labs(title = "Selected Data by County 2010 - 2018", color = "County") +
-  #     xlab("Year") + 
-  #     ylab("Percent Vacant") +
-  #     scale_color_viridis(discrete = TRUE) +
-  #     theme_minimal() +
-  #     theme(plot.title = element_text(hjust = 0.5))
-  # })
-
-  
 }
 
 
